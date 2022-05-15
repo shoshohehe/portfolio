ec2aac25a89128ea931d5f115c6d88d222c61e72
shoshohehe <65958978+shoshohehe@users.noreply.github.com>
2022-05-15 20:03:10 +0900

    Skill画像の追加

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,10 @@
+import GitImage from "../images/git.png";
+import RailsImage from "../images/rails.png";
+import ReactImage from "../images/react.png";
+import TerraformImage from "../images/terraform.png";
+import AwsImage from "../images/aws.png";
+import VueImage from "../images/vue.png";
+
 export const About = () => {
 	return (
 		<about>
@@ -7,7 +14,24 @@ export const About = () => {
 				<ul className="about-textlist">
 					<li>宮北 祥平</li>
 					<li>Shohei Miyakita</li>
-					<li>WebEngineer</li>
+                    <li>WebEngineer</li>
+                    <li>2020/04~</li>
+					<dl>
+						<dt>Skills</dt>
+						<dd>
+							<img src={RailsImage} alt="RailsImage" width="10%" height="100" padding-right="10"/>
+							<img src={AwsImage} alt="AwsImage" width="10%" height="100" padding-right="10"/>
+							<img
+								src={TerraformImage}
+								alt="TerraformImage"
+								width="10%"
+								height="100"
+							padding-right="10"/>
+							<img src={ReactImage} alt="ReactImage" width="10%" height="100" padding-right="10"/>
+							<img src={VueImage} alt="VueImage" width="10%" height="100" padding-right="10"/>
+							<img src={GitImage} alt="GitImage" width="10%" height="100" padding-right="10"/>
+						</dd>
+					</dl>
 				</ul>
 			</div>
 		</about>

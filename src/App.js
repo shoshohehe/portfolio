d93672e8183853c2e30b9763e1c55bbe9a8dc9ec
shoshohehe <65958978+shoshohehe@users.noreply.github.com>
2022-05-15 20:14:33 +0900

    Contactコンポーネントの追加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./style_sheets/App.css";
 import { Header } from "./components/Header";
 import { About } from "./components/About";
+import { Contact } from "./components/Contact";
 import React from "react";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 		<React.Fragment>
 			<Header />
 			<About />
+			<Contact/>
 		</React.Fragment>
 	);
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+export const Contact = () => {
+	return (
+		<about>
+			<div className="contact-container">
+				<h2 className="contact-h2">Contact</h2>
+				<hr />
+				<ul className="contact-snslist"></ul>
+			</div>
+		</about>
+	);
+};

08c66e5ed87e3f7e3ee58e50c8788aac662dd3d6
shoshohehe <65958978+shoshohehe@users.noreply.github.com>
2022-05-15 21:29:09 +0900

    資格情報の追加

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -4,6 +4,16 @@ export const Certificate = () => {
 			<div className="certificate-container">
 				<h2 className="certificate-h2">Certificate</h2>
 				<hr />
+				<dl className="certificate-textlist">
+					<dt>
+						<a href="https://www.credly.com/users/shohei-miyakita/badges">
+							AWS
+						</a>
+					</dt>
+					<dd>AWS Certified Solutions Architect – Associate</dd>
+					<dt>IPA</dt>
+					<dd>comming soon...</dd>
+				</dl>
 			</div>
 		</certificate>
 	);

141a21e961783a46fe52f2c9856865171d52d2fb
shoshohehe <65958978+shoshohehe@users.noreply.github.com>
2022-05-17 23:57:39 +0900

    iconの大きさ調整

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,24 +11,24 @@ import {
 export const About = () => {
 	return (
 		<about>
-			<div className="about-container">
+            <div className="about-container">
 				<h2 className="about-h2">About</h2>
 				<hr />
 				<ul className="about-textlist">
 					<li>宮北 祥平</li>
 					<li>Shohei Miyakita</li>
-                    <li>WebEngineer</li>
+					<li>WebEngineer</li>
 					<li>2020/04~</li>
 					<dl>
 						<dt>Skills</dt>
 						<dd>
-							<SiRuby size="2rem"/>
-							<SiRubyonrails size="2rem"/>
-							<SiTerraform size="2rem"/>
-                            <SiAmazonaws size="2rem"/>
-                            <SiReact size="2rem"/>
-                            <SiHtml5 size="2rem"/>
-                            <SiCss3 size="2rem"/>
+							<SiRuby size="4rem" />
+							<SiRubyonrails size="4rem" />
+							<SiTerraform size="4rem" />
+							<SiAmazonaws size="4rem" />
+							<SiReact size="4rem" />
+							<SiHtml5 size="4rem" />
+							<SiCss3 size="4rem" />
 						</dd>
 					</dl>
 				</ul>

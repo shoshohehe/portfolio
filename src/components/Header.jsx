373ecb4079ac2d3bb9faba4e1d53748975b84684
shoshohehe <65958978+shoshohehe@users.noreply.github.com>
2022-05-22 02:12:58 +0900

    Revert "Hamburger menu実装"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,11 @@
 import ProfileImage from "../images/profile-image.jpg";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { VscChromeClose } from "react-icons/vsc";
-
-import React, { useState } from "react";
 
 export const Header = () => {
-	const [buttonState, setState] = useState(true);
-
 	return (
 		<header
 			className="cover-image"
 			style={{ backgroundImage: `url(${ProfileImage})` }}
 		>
-			{buttonState === true ? (
-				<button
-					type="button"
-					onClick={() => setState(false)}
-					className="hamburger-menu"
-				>
-					<GiHamburgerMenu size="4rem" />
-				</button>
-			) : (
-				<button
-					type="button"
-					onClick={() => setState(true)}
-					className="hamburger-menu"
-				>
-					<VscChromeClose size="4rem" />
-				</button>
-			)}
 			<div className="header-container">
 				<h1 className="header-title">Shohei Miyakita</h1>
 			</div>
